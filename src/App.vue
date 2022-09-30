bf7d8778741fc83926f2a0a344379dcb4b936f34
adityar15 <adityask2194@gmail.com>
2022-09-30 20:05:54 +0100

    login code

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import {ref, defineAsyncComponent} from "vue"
 const GuestLayout = defineAsyncComponent(()=> import("./components/layouts/GuestLayout.vue"))
 const AuthLayout = defineAsyncComponent(()=> import("./components/layouts/AuthLayout.vue"))
 
-// components are used without registration thus need to take a object of layouts
+// components are used without registration thus need to take an object of layouts
 const layouts = {
   "GuestLayout": GuestLayout,
   "AuthLayout": AuthLayout,
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,25 @@
 <template>
   <CenterCard>
-    <Form class="w-full">
-      <FormGroup type="email" label="Email" :error="errorBag.email" />
-      <FormGroup type="password" label="Password" :error="errorBag.email" />
+    <Form class="w-full" @submit.prevent="login">
+      <FormGroup
+        type="email"
+        label="Email"
+        :error="errorBag.email" 
+        v-model="form.email"
+      />
+      <!-- <div>
+        <label>Email</label>
+        <input type="email" placeholder="Enter email" v-model="form.email"  />
+        <span>Email required</span>
+      </div> -->
+      <FormGroup
+        type="password"
+        label="Password"
+        :error="errorBag.password"
+        v-model="form.password"
+      />
       <div>
-        <span class="text-gray-700 underline underline-offset-2 cursor-pointer">Reset Password</span>
+        <span class="text-gray-700 underline underline-offset-2 cursor-pointer"> Reset Password </span>
       </div>
       <div class="text-center">
         <Button type="submit" class="w-36">Login</Button>
@@ -16,12 +31,42 @@
 <script setup>
 import CenterCard from "../components/organisms/CenterCard.vue";
 import { FormGroup, Form, Button } from "@adiranids/vue3-tailwind";
-import { reactive } from "@vue/reactivity";
+import { reactive } from "vue";
+import axios from 'axios'
 
 const errorBag = reactive({
   email: "",
   password: "",
 });
+
+const form = reactive({
+  email: "",
+  password: "",
+});
+
+function login() {
+  if (form.email == "") 
+  errorBag.email = "Please enter your email"
+  else 
+  errorBag.email = ""
+
+  if (form.password == "") 
+  errorBag.password = "Please enter your password"
+  else 
+  errorBag.password = ""
+
+  if (form.password != "" && form.email != "") {
+    errorBag.email = ""
+    errorBag.password = ""
+  }
+
+  axios.post("http://localhost:8000", form).then((response) => {
+
+  })
+
+  console.log("I am after request")
+
+}
 </script>
 
 <style scoped></style>
