764a7871d3621d1569d53da336768715a31c6743
adityar15 <adityask2194@gmail.com>
2022-09-30 20:10:28 +0100

    register code added

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,9 +1,9 @@
 <template>
   <CenterCard>
-    <Form class="w-full">
-      <FormGroup type="text" label="Full Name" :error="errorBag.name" />
-      <FormGroup type="email" label="Email" :error="errorBag.email" />
-      <FormGroup type="password" label="Password" :error="errorBag.email" />
+    <Form class="w-full" @submit.prevent="register">
+      <FormGroup type="text" label="Full Name" v-model="form.name" :error="errorBag.name" />
+      <FormGroup type="email" label="Email" v-model="form.email" :error="errorBag.email" />
+      <FormGroup type="password" label="Password" v-model="form.password" :error="errorBag.password" />
       <div class="text-center">
         <Button type="submit" class="w-36">Register</Button>
       </div>
@@ -15,12 +15,51 @@
 import CenterCard from "../components/organisms/CenterCard.vue";
 import { FormGroup, Form, Button } from "@adiranids/vue3-tailwind";
 import { reactive } from "@vue/reactivity";
+import axios from "axios"
 
 const errorBag = reactive({
   email: "",
   password: "",
   name: ""
 });
+
+const form = reactive({
+  email: "",
+  password: "",
+  name: ""
+});
+
+
+function register() {
+  if (form.email == "") 
+  errorBag.email = "Please enter your email"
+  else 
+  errorBag.email = ""
+
+  if (form.password == "") 
+  errorBag.password = "Please enter your password"
+  else 
+  errorBag.password = ""
+
+
+  if (form.name == "") 
+  errorBag.name = "Please enter your full name"
+  else 
+  errorBag.name = ""
+
+  if (form.password != "" && form.email != "" && form.name != "") {
+    errorBag.email = ""
+    errorBag.password = ""
+    errorBag.name = ""
+  }
+
+  // axios.post("http://localhost:8000", form).then((response) => {
+
+  // })
+
+  console.log("I am after request")
+
+}
 </script>
 
 <style scoped></style>
