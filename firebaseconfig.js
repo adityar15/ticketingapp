7a8142d297b8921d8e806a55c2ab49245bf6ff5c
adityar15 <adityask2194@gmail.com>
2022-10-14 16:31:42 +0100

    heroku url added

---
+++ firebaseconfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAreMxhb5aCO5sD7DAExtwYjhs_vl9qIUw",
+  authDomain: "vue3fastapi.firebaseapp.com",
+  projectId: "vue3fastapi",
+  storageBucket: "vue3fastapi.appspot.com",
+  messagingSenderId: "895746996038",
+  appId: "1:895746996038:web:47946e13ad216546809747"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/assets/api.js
+++ src/assets/api.js
@@ -3,7 +3,7 @@ import { getCookie } from "./cookie_helper";
 
 export const api = ()=>{
     return axios.create({
-        baseURL: 'http://127.0.0.1:8000',
+        baseURL: 'https://fastapi-code.herokuapp.com',
         withCredentials: true,
         headers: {
             "Accept": "application/json",
